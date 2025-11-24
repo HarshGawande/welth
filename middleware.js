7fb9dd017b85339b6dca599dfd698cedbabc7224
Harsh Gawande <138123246+HarshGawande@users.noreply.github.com>
2025-11-24 19:30:44 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,5 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
-import aj from "@/lib/arcjet"; // if you are using Arcjet
-import { createMiddleware } from "@arcjet/next"; // IMPORTANT
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
@@ -9,14 +7,12 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-const clerk = clerkMiddleware((auth, req) => {
+export default clerkMiddleware((auth, req) => {
   if (!auth().userId && isProtectedRoute(req)) {
     return auth().redirectToSignIn();
   }
   return NextResponse.next();
 });
-
-export default createMiddleware(aj, clerk);
 
 export const config = {
   matcher: [
